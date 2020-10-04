453d2ae3c1fa7e79deaf42cd8a130f4d1a375cfc
Daniel Venable <daniel.t.venable@gmail.com>
2020-10-03 17:10:14 -0700

    walls added

--- server.js
+++ server.js
@@ -10,13 +10,14 @@ const { Server } = require('ws'),
 class Game {
 	id = 0;
 	players = new Set;
+	terrain = terrains[Math.floor(Math.random() * terrains.length)];
 
 	constructor(ws, req) {
 		this.ticker = setInterval(this.tick.bind(this), Game.tick_length);
-		if (ws && req) this.add(ws, req);
+		if (ws && req) new Player(ws, this, get_username(req));
 	}
 
-	send_to_all(type, from, message = Symbol(), send_to_me = false, send_username = false) {
+	send_to_all(type, from, message, send_to_me = false, send_username = false) {
 		for (const player of this.players) {
 			if (!send_to_me && player === from) continue;
 			const obj = { type, message, id: from.id };
@@ -38,26 +39,26 @@ class Game {
 			player.position[0] += player.speed[0];
 			player.position[1] += player.speed[1];
 			this.send_to_all('position', player, player.position, true);
+			if (player.is_overlapping_wall()) {
+				this.send_to_all('die', player, Symbol(), true);
+				this.players.delete(player);
+				player.ws.close();
+			}
 		}
-	}
-
-	add(ws, req) {
-		new Player(ws, this, parse(req.url, true).query.username);
 	}
 
 	remove(player) {
 		this.players.delete(player);
-		this.send_to_all('bye', player);
+		this.send_to_all('bye', player, Symbol());
 	}
 
 	static max_players = Infinity;
 	static valid_speeds = new Set([-1, 0, 1]);
-	static speed = 10;
 	static tick_length = 50;
 }
 
 class Player {
-	position = [0, 0];
+	position = [20, 20];
 	speed = [0, 0];
 	color = random_color();
 
@@ -82,7 +83,7 @@ class Player {
 				data.type === 'position' &&
 				Game.valid_speeds.has(data.x) &&
 				Game.valid_speeds.has(data.y)) {
-				this.speed = [data.x * Game.speed, data.y * Game.speed];
+				this.speed = [data.x * Player.speed, data.y * Player.speed];
 			}
 		});
 
@@ -99,10 +100,36 @@ class Player {
 				username: player.username,
 				color: player.color
 			}));
+			ws.send(JSON.stringify({
+				type: 'wall',
+				message: game.terrain.walls
+			}));
 		}
 	}
 
-	static size = 50;
+	is_overlapping_wall() {
+		const [x, y] = this.position;
+		for (const [x1, y1, width, height] of this.game.terrain.walls) {
+			if (x + Player.radius > x1 && x - Player.radius < x1 + width &&
+				y + Player.radius > y1 && y - Player.radius < y1 + height) {
+				let is_below, is_right;
+				if ((is_right = x1 < x) && x < x1 + width ||
+					(is_below = y1 < y) && y < y1 + height ||
+					(is_right ? x1 + width - x : x1 - x) ** 2 +
+					(is_below ? y1 + height - y : y1 - y) ** 2 <
+					Player.sq_radius
+				) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	static radius = 5;
+	static sq_radius = Player.radius ** 2;
+	static speed = 2;
+	static max_name_length = 30;
 }
 
 const server = createServer(async (req, res) => {
@@ -115,13 +142,14 @@ const ws_server = new Server({ server });
 
 server.listen(port, () => console.log('Server running on http://localhost:%d', port));
 
-const games = [new Game];
+const games = [];
 
 ws_server.on('connection', (ws, req) => {
+	const username = get_username(req);
+	if (username.length > Player.max_name_length) return;
 	for (const game of games) {
 		if (game.players.size < Game.max_players) {
-			game.add(ws, req);
-			return;
+			return new Player(ws, game, username);
 		}
 	}
 	games.push(new Game(ws, req));
@@ -130,3 +158,44 @@ ws_server.on('connection', (ws, req) => {
 function random_color() {
 	return `hsl(${360 * Math.random()},100%,${Math.random() * 50 + 20}%)`;
 }
+
+function get_username({ url }) {
+	return String(parse(url, true).query.username);
+}
+
+const terrains = [
+	{
+		walls: [
+			[ 55, 45, 305, 1 ],   [ 650, 260, 5, 1 ],   [ 125, 260, 525, 1 ],
+			[ 530, 65, 1, 135 ],  [ 690, 125, 230, 1 ], [ 745, 145, 1, 410 ],
+			[ 240, 295, 465, 1 ], [ 390, 335, 1, 155 ], [ 490, 395, 155, 1 ],
+			[ 505, 505, 190, 1 ], [ 125, 380, 1, 175 ], [ 515, 470, 175, 1 ],
+			[ 485, 470, 185, 1 ], [ 90, 125, 320, 1 ],  [ 450, 45, 1, 135 ],
+			[ 240, 155, 1, 70 ],  [ 785, 85, 1, 25 ],   [ 610, 20, 1, 130 ],
+			[ 790, 235, 70, 1 ],  [ 885, 285, 1, 140 ], [ 780, 360, 40, 1 ],
+			[ 830, 565, 165, 1 ], [ 740, 625, 145, 1 ], [ 150, 585, 220, 1 ],
+			[ 240, 405, 1, 70 ],  [ 445, 440, 1, 275 ], [ 435, 415, 1, 310 ],
+			[ 505, 445, 115, 1 ], [ 485, 425, 1, 60 ],  [ 515, 455, 100, 1 ],
+			[ 695, 450, 1, 80 ],  [ 700, 460, 1, 120 ], [ 30, 680, 120, 1 ],
+			[ 35, 295, 75, 1 ],   [ 25, 100, 1, 145 ],  [ 35, 75, 1, 175 ],
+			[ 15, 365, 1, 80 ],   [ 40, 555, 75, 1 ],   [ 305, 695, 1, 175 ],
+			[ 0, 775, 125, 1 ],   [ 0, 855, 230, 1 ],   [ 5, 855, 195, 1 ],
+			[ 15, 845, 150, 1 ],  [ 1000, 0, 1, 995 ],  [ 995, 5, 1, 990 ],
+			[ 5, 5, 990, 1 ],     [ 5, 5, 1, 990 ],     [ 5, 995, 990, 1 ],
+			[ 5, 990, 990, 1 ],   [ 5, 995, 990, 1 ],   [ 5, 980, 985, 1 ],
+			[ 985, 0, 1, 975 ],   [ 990, 5, 1, 970 ],   [ 990, 5, 1, 970 ],
+			[ 995, 5, 1, 970 ],   [ 595, 720, 275, 1 ], [ 590, 550, 1, 60 ],
+			[ 455, 875, 440, 1 ], [ 765, 780, 1, 50 ],  [ 455, 775, 110, 1 ],
+			[ 240, 720, 40, 1 ],  [ 120, 920, 285, 1 ], [ 115, 905, 275, 1 ],
+			[ 215, 735, 1, 145 ], [ 195, 735, 1, 165 ], [ 195, 900, 1, 0 ],
+			[ 195, 900, 1, 5 ],   [ 485, 770, 1, 45 ],  [ 470, 645, 270, 1 ],
+			[ 535, 535, 1, 35 ],  [ 690, 490, 1, 100 ], [ 825, 455, 1, 125 ],
+			[ 920, 195, 1, 125 ], [ 790, 430, 1, 90 ],  [ 920, 195, 1, 125 ],
+			[ 995, 5, 1, 970 ],   [ 650, 775, 235, 1 ], [ 815, 815, 1, 50 ],
+			[ 940, 715, 1, 195 ], [ 940, 350, 1, 140 ], [ 890, 530, 100, 1 ],
+			[ 985, 25, 1, 955 ],  [ 975, 560, 1, 420 ], [ 915, 605, 60, 1 ],
+			[ 915, 595, 60, 1 ],  [ 915, 125, 1, 465 ], [ 915, 160, 60, 1 ],
+			[ 970, 5, 1, 155 ],   [ 910, 575, 70, 1 ],  [ 915, 590, 1, 5 ]
+		]
+	}
+];
