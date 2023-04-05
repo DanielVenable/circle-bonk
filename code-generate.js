2985ed4aa77bdd41a9746f7b2bbca82111806c53
Daniel Venable <daniel.venable@proton.me>
2023-04-05 16:14:45 -0700

    uses es6 modules

--- code-generate.js
+++ code-generate.js
@@ -1,26 +1,19 @@
-'use strict';
-
-const { createHash, randomBytes } = require('crypto'),
-	{ promisify } = require('util');
+import { createHash, randomBytes } from 'crypto';
+import { promisify } from 'util';
 
 const random = promisify(randomBytes);
 
-module.exports = {
-	async sign(head, body_size = 10, hash_size = 1) {
-		const body = (await random(body_size))
-			.toString('base64')
-			.slice(0, body_size)
-			.replace(/[/]/g, '_')
-			.replace(/[+]/g, '-');
-		return head + '-' + body + get_hash(head, body, hash_size);
-	},
-
-	verify(key, body_size = 10, hash_size = 1) {
-		const head = key.slice(0, -hash_size - body_size - 1),
-			body = key.slice(-hash_size - body_size, -hash_size),
-			hash = key.slice(-hash_size);
-		return get_hash(head, body, hash_size) === hash ? head : undefined;
-	}
+export async function sign(head, body_size = 10, hash_size = 1) {
+	const body = (await random(body_size))
+		.toString('base64')
+		.slice(0, body_size)
+		.replace(/[/]/g, '_')
+		.replace(/[+]/g, '-');
+	return head + '-' + body + get_hash(head, body, hash_size);
+}
+export function verify(key, body_size = 10, hash_size = 1) {
+	const head = key.slice(0, -hash_size - body_size - 1), body = key.slice(-hash_size - body_size, -hash_size), hash = key.slice(-hash_size);
+	return get_hash(head, body, hash_size) === hash ? head : undefined;
 }
 
 function get_hash(head, body, hash_size) {
